368fd0d63e323d26aa9510c73384390302d4c437
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2022-03-10 10:30:45 +0700

    added function scrapper

--- my_functions/scrape.js
+++ my_functions/scrape.js
@@ -1,3 +1,25 @@
+const getMetaData = require('metadata-scraper')
+
 exports.handler = async function(event, context) {
   // your server-side functionality
+  if (event.httpMethod === 'GET') {
+    const url = event.queryStringParameters.url || '';
+    try {
+      const result = await getMetaData(url);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result),
+      };
+    } catch (error) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: error.message }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({ message: 'not allowed' }),
+    };
+  }
 }
