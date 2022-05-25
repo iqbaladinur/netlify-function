9b16ead94065a4321923439abf78b627ff1ba146
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2022-05-25 16:11:35 +0700

    update function

--- my_functions/scrape.js
+++ my_functions/scrape.js
@@ -5,7 +5,10 @@ exports.handler = async function(event, context) {
   if (event.httpMethod === 'GET') {
     const url = event.queryStringParameters.url || '';
     try {
-      const result = await getMetaData(url);
+      const result = await getMetaData({
+        url,
+        ua: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+      });
       return {
         statusCode: 200,
         body: JSON.stringify(result),
